7ce065f3d4cc0fbef745e66690659b0e16eb3fe8
CRBroughton <crbroughton@posteo.uk>
2022-01-03 12:00:30 +0000

    added basic object type example

--- 02-tyescript-basics-and-basic-types/app.ts
+++ 02-tyescript-basics-and-basic-types/app.ts
@@ -1,17 +1,10 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+// const person: {
+//     name: string;
+//     age: number;
+// } 
+const person = {
+    name: 'Craig',
+    age: 31
+}
 
-    const result = n1 + n2;
-
-    if (showResult) {
-        console.log(phrase + result);
-    } else {
-        return result;
-    }
-};
-
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: '
-
-const result = add(number1, number2, printResult, resultPhrase);
+console.log(person.age)
