4b9357c098da42b84bd39aaacb02583d56b7581b
CRBroughton <crbroughton@posteo.uk>
2022-01-03 12:12:38 +0000

    added tuple type with notes

--- 02-tyescript-basics-and-basic-types/app.ts
+++ 02-tyescript-basics-and-basic-types/app.ts
@@ -1,11 +1,13 @@
-// const person: {
-//     name: string;
-//     age: number;
-// } 
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[]; // array of strings
+    role: [number, string]; // tuple type
+} = {
     name: 'Craig',
     age: 31,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
 
 console.log(person.name);
@@ -13,3 +15,6 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
 };
+
+// person.role.push('admin') - Push is an exception; Will be allowed by TS even though it is a tuple
+// person.role[1] = 10 - Tuple type cannot be overriden.
