56cc5c036115f8e891897badcd81b2cfee331b96
CRBroughton <crbroughton@posteo.uk>
2022-01-03 14:03:58 +0000

    added never return type example

--- 02-tyescript-basics-and-basic-types/app.ts
+++ 02-tyescript-basics-and-basic-types/app.ts
@@ -6,3 +6,10 @@ userInput = 'Craig'
 
 if (typeof userInput === 'string')
   userName = userInput
+
+function generateError(message: string, code: number): never {
+  // eslint-disable-next-line no-throw-literal
+  throw { message, errorCode: code }
+}
+
+generateError('An error occurred!', 500)
