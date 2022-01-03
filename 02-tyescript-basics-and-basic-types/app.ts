37506d4a0ec35617040a372a48d9fa23532aa779
CRBroughton <crbroughton@posteo.uk>
2022-01-03 12:21:13 +0000

    added enum example with notes

--- 02-tyescript-basics-and-basic-types/app.ts
+++ 02-tyescript-basics-and-basic-types/app.ts
@@ -1,13 +1,22 @@
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[]; // array of strings
-    role: [number, string]; // tuple type
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[]; // array of strings
+//     role: [number, string]; // tuple type
+// } = {
+//     name: 'Craig',
+//     age: 31,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+
+enum Role { ADMIN, READ_ONLY, AUTHOR }; // enums assign labels / constants to numbers / strings etc
+
+const person = {
     name: 'Craig',
     age: 31,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
 
 console.log(person.name);
@@ -15,6 +24,10 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
 };
+
+if (person.role === Role.ADMIN) {
+    console.log('Admin')
+}
 
 // person.role.push('admin') - Push is an exception; Will be allowed by TS even though it is a tuple
 // person.role[1] = 10 - Tuple type cannot be overriden.
