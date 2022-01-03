4bd8edf4cef8fde44ab0ec6c66e7d3f14f62013d
CRBroughton <crbroughton@posteo.uk>
2022-01-03 13:43:33 +0000

    added void example

--- 02-tyescript-basics-and-basic-types/app.ts
+++ 02-tyescript-basics-and-basic-types/app.ts
@@ -1,20 +1,9 @@
-type Combinable = number | string
-type ConverstionDescriptor = 'as-number' | 'as-text'
-
-function combine(input1: Combinable, input2: Combinable, resultConversion: ConverstionDescriptor) {
-  let result
-  if (typeof (input1 === 'number' && typeof input2 === 'number') || (resultConversion === 'as-number'))
-    result = +input1 + +input2
-  else
-    result = input1.toString() + input2.toString()
-  return result
+function add(n1: number, n2: number) {
+  return n1 + n2
 }
 
-const combinedAges = combine(30, 26, 'as-number')
-console.log(combinedAges)
+function printResult(num: number): void {
+  console.log(`Result: ${num}`)
+}
 
-const combinedStringAges = combine('30', '26', 'as-number')
-console.log(combinedStringAges)
-
-const combinedName = combine('Max', 'Anna', 'as-text')
-console.log(combinedName)
+printResult(add(5, 12))
