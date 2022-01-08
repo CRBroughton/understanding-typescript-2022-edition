e507e95b6ab1d1738cd29e77f360e6a8c58a7645
CRBroughton <crbroughton@posteo.uk>
2022-01-08 14:38:21 +0000

    added rest params examples

--- 04-next-generation-javascript-and-typescript/src/app.ts
+++ 04-next-generation-javascript-and-typescript/src/app.ts
@@ -21,29 +21,46 @@
 
 
 // single expression arrow function
-const add = (a: number, b: number = 1) => a + b
+// const add = (a: number, b: number = 1) => a + b
 
-console.log(add(2, 5))
+// console.log(add(2, 5))
 
-const printOutput: (a: number | string) => void = output => console.log(output)
+// const printOutput: (a: number | string) => void = output => console.log(output)
 
-const button = document.querySelector('button')
+// const button = document.querySelector('button')
 
-if (button) {
-  button.addEventListener('click', event => console.log(event))
+// if (button) {
+//   button.addEventListener('click', event => console.log(event))
+// }
+// printOutput(add(5))
+
+// // Spread Operator
+
+// const hobbies = ['Sports', 'Cooking']
+// const activeHobbies = ['Hiking']
+
+// activeHobbies.push(...hobbies)
+
+// const person = {
+//   name: 'Craig',
+//   age: 31
+// }
+
+// const copiedPerson = { ...person }
+
+// Rest Parameters
+
+const add = (...numbers: number[]) => { // Accepts an unlimited amount of numbers by the spread operator, turning the params into an array
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue
+  }, 0);
 }
-printOutput(add(5))
 
-// Spread Operator
-
-const hobbies = ['Sports', 'Cooking']
-const activeHobbies = ['Hiking']
-
-activeHobbies.push(...hobbies)
-
-const person = {
-  name: 'Craig',
-  age: 31
+const addTuple = (...numbers: [number, number, number]) => { // Accepts a limited amount of numbers by the spread operator, turning the params into an array
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue
+  }, 0);
 }
 
-const copiedPerson = { ...person }
+const addNumbers = add(5, 10, 2, 3.7)
+console.log(addNumbers)
