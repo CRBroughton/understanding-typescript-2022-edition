b7d3dbef17a43fd2db65fc8e93c1f45906190345
CRBroughton <crbroughton@posteo.uk>
2022-01-08 14:04:34 +0000

    completed let and const section

--- 04-next-generation-javascript-and-typescript/src/app.ts
+++ 04-next-generation-javascript-and-typescript/src/app.ts
@@ -1,1 +1,17 @@
-// Code goes here!
+const userName = 'Craig'
+
+let age = 30
+
+age = 31
+
+function add(a: number, b: number) {
+  let result
+  result = a + b
+  return result
+}
+
+if (age > 30) {
+    let isOld = true
+}
+
+console.log(isOld)
