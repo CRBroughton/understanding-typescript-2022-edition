3032918691ed06de22e97d4a96ee45f20a3c43cb
CRBroughton <crbroughton@posteo.uk>
2022-01-13 13:59:40 +0000

    added employees array and methods

--- 05-classes-and-interfaces/src/app.ts
+++ 05-classes-and-interfaces/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
     name: string
+    employees: string[] = []
 
     constructor(n:string) { 
         this.name = n
@@ -8,12 +9,26 @@ class Department {
     describe(this: Department) { // Refer to instance based on Department class
         console.log('Department: ' + this.name)
     }
+    
+    addEmployee(employee: string) {
+        this.employees.push(employee)
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length)
+        console.log(this.employees)
+    }
 }
 
 const accounting = new Department('Accounting')
 
+accounting.addEmployee('Craig')
+accounting.addEmployee('Max')
+
+
 accounting.describe()
+accounting.printEmployeeInformation()
 
-const accountingCopy = { describe: accounting.describe }
+// const accountingCopy = { describe: accounting.describe }
 
-accountingCopy.describe()
+// accountingCopy.describe()
