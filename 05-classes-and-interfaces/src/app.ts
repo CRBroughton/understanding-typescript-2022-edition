591647db115c66d566e05dd567b26a0b977040b0
CRBroughton <crbroughton@posteo.uk>
2022-01-13 17:09:35 +0000

    completed extended interfaces section

--- 05-classes-and-interfaces/src/app.ts
+++ 05-classes-and-interfaces/src/app.ts
@@ -1,10 +1,12 @@
-interface Greetable {
+interface Named { 
     readonly name: string
+}
 
+interface Greetable extends Named {
     greet(phrase: string): void
 }
 
-class Person implements Greetable {
+class Person implements Greetable { // multiple interfaces can be here
     name: string
     age = 30
 
