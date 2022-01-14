351d09968502dac8c45a4e57d98bc5161dc80f95
CRBroughton <crbroughton@posteo.uk>
2022-01-14 10:35:36 +0000

    completed intersection types section

--- 06-advanced-types/src/app.ts
+++ 06-advanced-types/src/app.ts
@@ -1,1 +1,19 @@
-// Code goes here!
+type Admin = {
+    name: string
+    priviledges: string[]
+}
+
+type Employee = {
+    name: string
+    startDate: Date
+}
+
+type ElevatedEmployee = Admin & Employee // Intersection type; Combine two or more types
+
+// interface ElevatedEmployee extends Employee, Admin {} // - Alternative Intersection type
+
+const el: ElevatedEmployee = {
+    name: 'Craig',
+    priviledges: ['create-server'],
+    startDate: new Date()
+}
