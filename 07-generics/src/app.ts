35baadfb4611b11e336bbafb62158f764d8b89ea
CRBroughton <crbroughton@posteo.uk>
2022-01-28 11:28:37 +0000

    finished generic classes

--- 07-generics/src/app.ts
+++ 07-generics/src/app.ts
@@ -70,3 +70,36 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 }
 
 extractAndConvert({name: 'Craig'}, 'name')
+
+
+// ---
+
+// A generic class using generic types
+
+class DataStorage<T extends string | number | boolean> {
+    private data: T[] = []
+
+    addItem(item: T) {
+        this.data.push(item)
+    }
+
+    removeItem(item: T) {
+        this.data.splice(this.data.indexOf(item), 1)
+    }
+
+    getItems() {
+        return [...this.data]
+    }
+}
+
+const textStorage = new DataStorage<string>()
+
+textStorage.addItem('10')
+textStorage.addItem('11')
+textStorage.removeItem('10')
+console.log(textStorage.getItems())
+
+// ---
+
+// Below will not work as the DataStorage only allows certain types
+// const objStorage = new DataStorage<object>()
