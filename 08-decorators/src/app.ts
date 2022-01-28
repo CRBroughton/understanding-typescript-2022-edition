2596fbe5e46e025f3bd379415edbee2d249b351b
CRBroughton <crbroughton@posteo.uk>
2022-01-28 14:31:33 +0000

    completed autobind section

--- 08-decorators/src/app.ts
+++ 08-decorators/src/app.ts
@@ -126,3 +126,35 @@ class Product {
         return this._price * (1 + tax)
     }
 }
+
+// ----------------------------------------------------------------
+
+// -- Autobind Decorator example
+
+function Autobind(_target: any, _methodName: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this)
+            return boundFn
+        }
+    }
+    return adjDescriptor
+}
+
+class Printer {
+    message = 'This works!'
+
+    @Autobind
+    showMessage() {
+        console.log(this.message)
+    }
+}
+
+const p = new Printer()
+
+const button = document.querySelector('button')!
+
+button.addEventListener('click', p.showMessage)
