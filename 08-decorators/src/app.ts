e2e15abbf92bddef6961b4fe4c324e6fc43ad89f
CRBroughton <crbroughton@posteo.uk>
2022-01-28 12:21:08 +0000

    completed second decorator

--- 08-decorators/src/app.ts
+++ 08-decorators/src/app.ts
@@ -22,14 +22,41 @@
 
 // Factory Decorators - returns a function
 
-function Logger(logString: string) {
-    return function(constructor: Function) {
-    console.log(logString)
-    console.log(constructor)
+// function Logger(logString: string) {
+//     return function(constructor: Function) {
+//     console.log(logString)
+//     console.log(constructor)
+//     }
+// }
+
+
+
+// @Logger('LOGGING - PERSON')
+// class Person {
+//     name = "Craig"
+
+//     constructor() {
+//         console.log('Create person object...')
+//     }
+// }
+
+// const pers = new Person();
+// console.log(pers)
+
+// --- Another example of a factory
+
+function WithTemplate(template: string, hookId: string) {
+    return function(constructor: any) {
+        const hookEl = document.getElementById(hookId);
+
+        const p = new constructor()
+        if (hookEl) 
+            hookEl.innerHTML = template;
+            hookEl!.querySelector('h1')!.textContent = p.name
     }
 }
 
-@Logger('LOGGING - PERSON')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
     name = "Craig"
 
