dd168a96afe6078a640e3ed0e4b12a3209a7fe51
CRBroughton <crbroughton@posteo.uk>
2022-01-28 12:14:26 +0000

    completed first factory function

--- 08-decorators/src/app.ts
+++ 08-decorators/src/app.ts
@@ -1,11 +1,35 @@
 // This function is a decorator
 // Decorators execute when the class is defined, not instantiated.
-function Logger(constructor: Function) {
-    console.log('Logging...')
+// function Logger(constructor: Function) {
+//     console.log('Logging...')
+//     console.log(constructor)
+// }
+
+// @Logger
+// class Person {
+//     name = "Craig"
+
+//     constructor() {
+//         console.log('Create person object...')
+//     }
+// }
+
+// const pers = new Person();
+// console.log(pers)
+
+
+// ----------------------------------------------------------------
+
+// Factory Decorators - returns a function
+
+function Logger(logString: string) {
+    return function(constructor: Function) {
+    console.log(logString)
     console.log(constructor)
+    }
 }
 
-@Logger
+@Logger('LOGGING - PERSON')
 class Person {
     name = "Craig"
 
