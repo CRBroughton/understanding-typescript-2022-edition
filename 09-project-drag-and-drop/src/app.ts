29372bcf7e758fea543674c8640cf46db7e60fad
CRBroughton <crbroughton@posteo.uk>
2022-01-31 13:16:46 +0000

    ProjectState instance is now static

--- 09-project-drag-and-drop/src/app.ts
+++ 09-project-drag-and-drop/src/app.ts
@@ -2,6 +2,18 @@
 
 class ProjectState {
     private projects: any[] = []
+    private static instance: ProjectState
+
+    private constructor() {
+
+    }
+
+    static getInstance() {
+        if (this.instance)
+            return this.instance
+        this.instance = new ProjectState()
+        return this.instance
+    }
 
     addProject(title: string, description: string, numOfPeople: number) {
         const newProject = {
@@ -14,7 +26,7 @@ class ProjectState {
     }
 }
 
-const projectState = new ProjectState()
+const projectState = ProjectState.getInstance()
 
 // Validator
 interface Validatable {
