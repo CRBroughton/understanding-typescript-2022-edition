3dc6ebcf000fd4140bad6849a620e24980bb7322
CRBroughton <crbroughton@posteo.uk>
2022-01-31 15:19:15 +0000

    refactored with generics

--- 09-project-drag-and-drop/src/app.ts
+++ 09-project-drag-and-drop/src/app.ts
@@ -14,15 +14,23 @@ class Project {
 
 // Project State Management
 
-type Listener = (items: Project[]) => void
+type Listener<T> = (items: T[]) => void
 
-class ProjectState {
-    private listeners: Listener[] = []
+class State<T> {
+    protected listeners: Listener<T>[] = []
+
+    addListener(listenerFn: Listener<T>) {
+        this.listeners.push(listenerFn)
+    }
+    
+}
+
+class ProjectState extends State<Project> {
     private projects: Project[] = []
     private static instance: ProjectState
 
     private constructor() {
-
+        super()
     }
 
     static getInstance() {
@@ -32,9 +40,7 @@ class ProjectState {
         return this.instance
     }
 
-    addListener(listenerFn: Listener) {
-        this.listeners.push(listenerFn)
-    }
+
 
     addProject(title: string, description: string, numOfPeople: number) {
         const newProject = new Project(
