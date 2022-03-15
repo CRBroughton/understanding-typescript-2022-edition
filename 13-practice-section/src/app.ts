8b9f4f6407e0524f7895f43bdcbdf0ab4169d7a7
CRBroughton <crbroughton@posteo.uk>
2022-03-15 19:52:54 +0000

    created addressInput const

--- 13-practice-section/src/app.ts
+++ 13-practice-section/src/app.ts
@@ -1,7 +1,12 @@
 const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
 
 function searchAddressHandler(event: Event) {
     event.preventDefault();
+
+    const enteredAddress = addressInput.value;
+
+    // send to Google API
 };
 
 form.addeventListener('submit', searchAddressHandler);
