c4fe4aa6d2eb1608b4f7e754a9ecb8cddeb2ad16
CRBroughton <crbroughton@posteo.uk>
2022-04-17 13:36:04 +0100

    added submit handler

--- 14-react-and-typescript/src/components/NewTodo.tsx
+++ 14-react-and-typescript/src/components/NewTodo.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const NewTodo: React.FC = () => {
-  return <form>
-    <div>
-      <label htmlFor="todo-text">Todo Text</label>
-      <input type="text" id="todo-text" placeholder=""/>
-    </div>
-    <button type="submit">ADD TODO</button>
-  </form>
+  const textInputRef = useRef<HTMLInputElement>(null)
+
+  const todoSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault()
+    const enteredText = textInputRef.current!.value
+
+    console.log(enteredText)
+  }
+
+  return (
+    <form onSubmit={todoSubmitHandler}>
+      <div>
+        <label htmlFor="todo-text">Todo Text</label>
+        <input type="text" id="todo-text" placeholder="" ref={textInputRef}/>
+      </div>
+      <button type="submit">ADD TODO</button>
+    </form>
+  )
 }
 
 export default NewTodo
