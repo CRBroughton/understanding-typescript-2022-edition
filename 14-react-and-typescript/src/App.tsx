3021b3b6322315289f8350d2c9ebff26c6840058
CRBroughton <crbroughton@posteo.uk>
2022-04-17 13:23:03 +0100

    imported TodoList into App.tsx

--- 14-react-and-typescript/src/App.tsx
+++ 14-react-and-typescript/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import TodoList from './components/TodoList'
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <TodoList />
     </div>
   )
 }
