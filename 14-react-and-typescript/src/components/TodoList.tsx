08d6fd63db5ae0303481274b0eb43ad18e0bc81d
CRBroughton <crbroughton@posteo.uk>
2022-04-17 13:54:38 +0100

    added delete button and handler

--- 14-react-and-typescript/src/components/TodoList.tsx
+++ 14-react-and-typescript/src/components/TodoList.tsx
@@ -5,13 +5,16 @@ interface Props {
     id: string
     text: string
   }[]
+  onDeleteTodo: (todoId: string) => void
 }
 
 const TodoList: React.FC<Props> = (props) => {
   return <ul>
     {props.items.map(item =>
       <li
-        key={item.id}>{item.text}
+        key={item.id}>
+        <span>{item.text}</span>
+        <button onClick={props.onDeleteTodo.bind(null, item.id)}>DELETE</button>
       </li>,
     )}
   </ul>
