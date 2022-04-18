b68bb18f941f6efd648934f446eac27c6ac11a98
CRBroughton <crbroughton@posteo.uk>
2022-04-18 17:25:47 +0100

    created error middleware with interface

--- 15-nodejs-express-and-typescript/src/app.ts
+++ 15-nodejs-express-and-typescript/src/app.ts
@@ -1,5 +1,21 @@
 import express from 'express'
+import type { NextFunction, Request, Response } from 'express'
+import todoRoutes from './routes/todos'
+
+interface ExpressError {
+  err: Error
+  req: Request
+  res: Response
+  next: NextFunction
+}
 
 const app = express()
+app.use('/todos', todoRoutes)
+
+app.use(({ ...args }: ExpressError) => {
+  args.res.status(500).json({ message: args.err.message })
+})
 
 app.listen(3001)
+
+// err: Error, req: Request, res: Response, next: NextFunction
