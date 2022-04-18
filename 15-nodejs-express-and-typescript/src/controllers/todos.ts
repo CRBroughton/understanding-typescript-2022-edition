278098562235376edeffff77e2d965771397cd6d
CRBroughton <crbroughton@posteo.uk>
2022-04-18 17:34:51 +0100

    added successful response

--- 15-nodejs-express-and-typescript/src/controllers/todos.ts
+++ 15-nodejs-express-and-typescript/src/controllers/todos.ts
@@ -8,4 +8,6 @@ export const createTodo: RequestHandler = (req, res, next) => {
   const newTodo = new Todo(Math.random().toString(), text)
 
   TODOS.push(newTodo)
+
+  res.status(201).json({ message: 'Created the todo.', createdTodo: newTodo })
 }
